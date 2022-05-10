0807020fbac4a0c3f4d83bd12b032c47ce9a1ff3
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-05-09 22:21:30 -0300

    Create (Não)ÉMaiorQue10.java

---
+++ (Não)ÉMaiorQue10.java
@@ -0,0 +1,19 @@
+package Exercícios14_ao_43;
+import java.util.*;
+
+public class Exercicio_14 {
+	public static void main(String[] args) {
+		int x = 30;
+		int y = 10;
+		
+		
+		
+		if (x>y) {
+			System.out.println("É MAIOR QUE 10!");
+		} else {
+			System.out.println("NÃO É MAIOR QUE 10!");
+		}
+		
+		
+	}
+}
