a53e45fe74d3140d892f68f7bf2bf58a16961268
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-05-15 12:13:57 -0300

    Update Comissão.java

--- Comissão.java
+++ Comissão.java
@@ -1,3 +1,7 @@
+//Ler o salário fixo e o valor das vendas efetuadas pelo vendedor de uma empresa. Sabendo-se que
+//ele recebe uma comissão de 3% sobre o total das vendas até R$ 1.500,00 mais 5% sobre o que
+//ultrapassar este valor, calcular e escrever o seu salário total. 
+
 package Exercicios14ao43;
 import java.util.*;
 
