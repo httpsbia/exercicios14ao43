d3ca7d0f270e091da59cf3325f0c58a2fcafc29d
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-05-15 12:29:48 -0300

    Update Timess.java

--- Timess.java
+++ Timess.java
@@ -1,3 +1,6 @@
+//Ler o nome de 2 times e o número de gols marcados na partida (para cada time). Escrever o nome
+//do vencedor. Caso não haja vencedor deverá ser impressa a palavra EMPATE. 
+
 package Exercicios14ao43;
 	import java.util.*;
 	
