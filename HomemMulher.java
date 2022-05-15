5ca13aac3fab5d0ccb21069046c1005395dab5e4
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-05-15 12:41:22 -0300

    Update HomemMulher.java

--- HomemMulher.java
+++ HomemMulher.java
@@ -1,3 +1,8 @@
+//Escreva um algoritmo que leia as idades de 2 homens e de 2 mulheres (considere que as idades
+//dos homens serão sempre diferentes entre si, bem como as das mulheres). Calcule e escreva a soma
+//das idades do homem mais velho com a mulher mais nova, e o produto das idades do homem mais
+//novo com a mulher mais velha. 
+
 package Exercicios14ao43;
     import java.util.*;
 
