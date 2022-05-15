5d674fbbb690c924483dbc2355f2e574c3399368
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-05-15 12:10:11 -0300

    Update PesoIdeal.java

--- PesoIdeal.java
+++ PesoIdeal.java
@@ -1,3 +1,8 @@
+//Enunciado: Tendo como dados de entrada o nome, a altura e o sexo (M ou F) de uma pessoa, calcule
+//e mostre seu peso ideal, utilizando as seguintes fórmulas:
+// - para sexo masculino: peso ideal = (72.7 * altura) - 58
+// - para sexo feminino: peso ideal = (62.1 * altura) - 44.7
+
 package Exercícios14_ao_43;
 import java.util.*;
 
