c5b4912958be66f37c2a9193ce48aa8ab35c48c2
AnaBeatriz <104109534+httpsbia@users.noreply.github.com>
2022-05-15 12:15:43 -0300

    Update SaldoAtual.java

--- SaldoAtual.java
+++ SaldoAtual.java
@@ -1,3 +1,7 @@
+//Faça um algoritmo para ler: número da conta do cliente, saldo, débito e crédito. Após, calcular e
+//escrever o saldo atual (saldo atual = saldo - débito + crédito). Também testar se saldo atual for maior
+//ou igual a zero escrever a mensagem 'Saldo Positivo', senão escrever a mensagem 'Saldo Negativo'. 
+
 package Exercicios14ao43;
 	import java.util.*;
 	
